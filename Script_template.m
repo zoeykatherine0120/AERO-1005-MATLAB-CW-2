c36327ee06a3c7f12e57ff4a50f2e722cb733cc3
zoeykatherine0120 <zoeykatherine0120@gmail.com>
2026-04-27 16:39:36 +0800

    Add files via upload

---
+++ Script_template.m
@@ -0,0 +1,27 @@
+% Insert name here
+% Muyun ZHONG
+% Insert email address here
+% ssymz8@nottingham.edu.cn
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
+
+% Insert answers here
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% Insert answers here
